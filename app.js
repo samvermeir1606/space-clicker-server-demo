a538a3dafbce2499d7a16fec094a3180c445aaa3
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-07 14:38:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 app.get('/database/showall',function(req,res){
 	client.query('SELECT * FROM ${tableName};', (err, outcome) => {   
 		if (err) throw err;
-		else {
+		else { 
 			res.send(outcome)
 			console.log("ShowAll requested: responded SUCCESS")
 		}
