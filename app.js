542d5dcde1a92c3c2bf9b51cb46baad2ced56315
Sam Vermeir <sam.vermeir@hotmail.com>
2021-04-08 11:22:18 +0200

    added create player endpoint

--- app.js
+++ app.js
@@ -27,12 +27,83 @@ app.get('/', (req, res) => {
 	console.log("Get request for `/` received")
 })
 
+
+// PLAYER ENDPOINTS
+app.get('/player/create/:username',function(req,res){
+	var username=req.params.username;
+	console.log("Create Player Request received for username: "+username)
+	var UserExists=false;
+	// check if player already exists
+	client.query("SELECT * FROM userlist WHERE username = '"+username+"';", (err, outcome) => {   
+		if (err) throw err;
+		else {
+			if (outcome.rows.length>0) {
+				UserExists=true;
+			}
+		}
+	})
+
+	if (UserExists) {
+		res.send("User already exists")
+		console.log("This user already exists...")
+	}
+	else {
+		var accountcreated=	var formatedMysqlString = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() - ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
+		var lastonline=	var formatedMysqlString = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() - ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
+		var highscoreposted=	var formatedMysqlString = (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() - ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ');
+	
+		client.query("INSERT INTO userlist(username, displayname, accountcreated, lastonline, score, highscoreposted, banned) VALUES ('"+username+"','NoNameYet',"+accountcreated+","+lastonline+", 0,"+highscoreposted+",0);", (err, outcome) => {   
+			if (err) throw err;
+			else {
+				res.send("User Created in database")
+				console.log("User Created in database")
+			}
+		})
+	}
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+//DATABASE Endpoints
 app.get('/database/showall',function(req,res){
 	client.query("SELECT * FROM userlist;", (err, outcome) => {   
 		if (err) throw err;
 		else {
-			res.send(outcome)
+			res.send(outcome.rows)
 			console.log("ShowAll requested: responded SUCCESS")
+		}
+	})
+})
+
+app.get('/database/deleteall',function(req,res){
+	client.query("DELETE FROM userlist WHERE userid > 0;", (err, outcome) => {   
+		if (err) throw err;
+		else {
+			res.send(outcome)
+			console.log("DELETE ALL requested: responded SUCCESS")
+		}
+	})
+})
+
+app.get('/database/deleteone/:userid',function(req,res){
+	client.query("DELETE FROM userlist WHERE userid = "+req.params.userid+";", (err, outcome) => {   
+		if (err) throw err;
+		else {
+			res.send(outcome)
+			console.log("DELETE ONE requested: responded SUCCESS")
 		}
 	})
 })
